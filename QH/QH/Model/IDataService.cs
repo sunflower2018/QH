d84bbd6381565b007300b7f0e2a00e8468698cb2
sunflower2018 <donglizhog@163.com>
2018-04-17 13:31:12 +0800

    1

---
+++ QH/QH/Model/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QH.Model
+{
+    public interface IDataService
+    {
+        void GetData(Action<DataItem, Exception> callback);
+    }
+}
